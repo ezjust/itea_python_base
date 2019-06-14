7d753be43a67b43f77ebcd573a910da75865110b
ezjust <3spirit3@ukr.net>
2019-06-14 20:30:24 +0300

    second try, changed to PEP8, corrected some variable names and TYPO mistakes

---
+++ first_lesson_tasks.py
@@ -0,0 +1,30 @@
+#1 task
+a = 3 ^ 128
+b = 2 ^ 128
+c = b * a
+
+#2 task
+print('python\n'*100)
+
+#3 task
+a = 'Напишите программу, которая считает сколько букв ‘o’ в заданной строке'
+a.count("о")
+
+#4 task
+a = [1, 2, 192, 15]
+a.sort()
+print('sorted a:', a)
+#5 task
+a = input('insert a:')
+b = input('insert b:')
+print('sum of last two values: ', int(a[-1])+int(b[-1]))
+
+#6 task
+a = 78
+print('78 celsium degrees in Kelvin: ', a-273)
+
+#7 task
+a = 'one, two, three, five, 11, eleven'
+list_a = list(a.split(','))
+
+print(list_a)
