163e68af61365681055ccdda8a7833ce10bd2fb2
ezjust <3spirit3@ukr.net>
2019-06-24 14:27:06 +0300

    4th lesson, 1file = 1task, made 3 tasks

---
+++ gen_random_list.py
@@ -0,0 +1,23 @@
+import random
+
+list_len = int(input('Set value:'))
+random_list = []
+last_same_counter = []
+last_number = []
+for i in range(0, list_len):
+    random_list.append(random.randint(0, 1))
+print(random_list)
+count_same = 1
+for i in range(0, len(random_list) - 1):
+    print(random_list[i], random_list[i + 1])
+    if random_list[i] == random_list[i + 1]:
+        count_same += 1
+    else:
+        last_same_counter.append(count_same)
+        count_same = 1
+last_same_counter.append(count_same)
+print(last_same_counter)
+print('Maximum same characters in a row:', max(last_same_counter))
+
+
+
--- sum_matrix_diagonal_elements.py
+++
@@ -1,9 +0,0 @@
-# Найти сумму диагональных элементов матрицы
-
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-counter = 0
-summation = 0
-for raw in matrix:
-    counter += 1
-    summation += raw[len(raw)-counter] + raw[-1 + counter]
-print('Sum of matrix diagonal =', summation)
