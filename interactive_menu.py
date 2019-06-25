9ab3f14cd51f800f9cb34ce384e8196209dd84e9
ezjust <3spirit3@ukr.net>
2019-06-25 16:59:03 +0300

    Update interactive_menu.py
    Co-Authored-By: itea-artem <51487178+itea-artem@users.noreply.github.com>

--- interactive_menu.py
+++ interactive_menu.py
@@ -45,7 +45,7 @@ def manage_telephone_book(tel_dictionary):
     elif value == 3:
         print('There are such records in this book:')
         counter = 0
-        for key, values in tel_dictionary.items():
+        for key, value in tel_dictionary.items():
             print('Record #{} Name: {}   Phone: {}'.format(counter, key, values))
             counter += 1
         record_num = int(input("Input number of record to change:"))
