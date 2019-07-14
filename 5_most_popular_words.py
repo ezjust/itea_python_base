1931627da99fc91c96b4e8c4f12a8f49634e5426
ezjust <3spirit3@ukr.net>
2019-07-14 20:13:56 +0300

    changed logic of 5_most_popular_words file

--- 5_most_popular_words.py
+++ 5_most_popular_words.py
@@ -2,12 +2,13 @@ from collections import Counter
 
 words_count = []
 with open('test1.txt') as file1:
-    counter = Counter()
     for line in file1:
-        line = line.strip().split('\t')
-        words = line[0].split()
-        counter.update(words)
-    top5 = [word[0] for word in counter.most_common(5)]
+        for element in line.split():
+            if element is not '':
+                words_count.append(element)
+words = Counter(words_count)
+top5 = words.most_common(5)
+print(top5)
 with open('test1_result.txt', 'w') as result_file:
     for word in top5:
-        result_file.write(word + '\n')
+        result_file.write(word[0] + '\n')
